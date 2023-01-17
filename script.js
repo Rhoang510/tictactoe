0f3c32a27a2101e25f273617414e6c3787228709
Roger <rhoang510@gmail.com>
2023-01-17 11:42:22 -0800

    Fixed modal

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const Gameboard = () => {
+const player1Name = document.querySelector('.player1')
+const player2Name = document.querySelector('.player2')
+
+const Gameboard = (() => {
     let board = new Array(9).fill('')
-    const getBoard = () => { [...board], console.log([...board])}
-    const setValue = (index, marker) => { board[index] = marker, console.log([...board]) }
-    const resetBoard = () => { board.fill('') }
+    const getBoard = () => board
+    const getValue = (index) => board[index]
+    const setValue = (index, marker) => { board[index] = marker, console.log(board) }
+    const resetBoard = () => { board.fill(''), console.log(board) }
     return {
+        getBoard,
         setValue,
         resetBoard,
-        getBoard
+        getValue
     }
-}
+})()
 
 const Player = (name, marker) => {
     const getName = () => name
@@ -17,30 +22,37 @@ const Player = (name, marker) => {
 }
 
 const gameFlow = (player1Name, player2Name) => {
-    const square = Array.from(document.querySelectorAll('.square'))
-    let gameboard = Gameboard()
+    const square = document.querySelectorAll('.square')
+    const resetBtn = document.querySelector('.reset-btn')
+    let gameboard = Gameboard
     let player1 = Player(player1Name, 'X')
     let player2 = Player(player2Name, 'O')
     let currentPlayer = player1
+    let gameover = false
 
-    const seePlayer =(() => {
-        console.log(player1.getName())
-        console.log(player1.getMarker())
-        console.log(player2.getName())
-        console.log(player2.getMarker())
-    })()
+    displayController.whosTurn(currentPlayer)
     
     const switchPlayers = () => {
-        if(currentPlayer === player1) {
-            currentPlayer = player2
-        } else {
-            currentPlayer = player1
-        }
+        currentPlayer = currentPlayer === player1 ? player2 : player1
     }
 
-    const checkForWin = (marker) => {
-        const winner = winConditions.find(winCondition => winCondition.every(index => gameboard[index] === marker))
-        winner === null ? false : true
+    const checkForWin = (player) => {
+        const draw = gameboard.getBoard().every(square => square != '')
+        const winner = winConditions
+        .find(winCondition => winCondition
+            .every(index => gameboard.getBoard()[index] === player.getMarker()))
+
+        if (winner != null) {
+            displayController.openModal()
+            displayController.displayWinner(currentPlayer)
+            gameover = true
+            resetGameModal()
+        } if (winner != null && draw) {
+            displayController.openModal()
+            displayController.displayDraw()
+            gameover = true
+            resetGameModal()
+        }
     }
 
     const winConditions = [
@@ -54,51 +66,97 @@ const gameFlow = (player1Name, player2Name) => {
         [2, 4, 6],
     ]
 
-    
     const choosingSquare = square.forEach(square => {
         square.addEventListener('click', (e) => {
             let index = e.target.dataset.index
-            gameboard.setValue(index, currentPlayer.getMarker())
-            square.textContent = currentPlayer.getMarker()
-            switchPlayers()
-            checkForWin(currentPlayer.getMarker())
-            console.log(currentPlayer.getName())
-            console.log(index)
+            if (square.textContent === '' && gameover === false) {
+                gameboard.setValue(index, currentPlayer.getMarker())
+                displayController.renderBoard()
+                checkForWin(currentPlayer)
+                switchPlayers()
+            } if (square.textContent != '' && gameover === false) {
+                displayController.whosTurn(currentPlayer)
+            }
         })
     })
     
+    const resetGame = () => {
+        gameboard.resetBoard()
+        displayController.closeModal()
+        currentPlayer = player1
+        gameover = false
+        displayController.whosTurn(currentPlayer)
+        square.forEach(square => {
+            square.textContent = ''
+        })
+    }
+
+    const resetGameBtn = (() => {
+        resetBtn.addEventListener('click', (e) => {
+            resetGame()
+        })
+    })()
+
+    const resetGameModal = () => {
+        const modal = document.querySelector('.modal')
+        window.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                resetGame()
+            }
+        })
+    }
+
     return {
         switchPlayers,
         currentPlayer,
-        checkForWin,
         gameboard,
-        choosingSquare
+        choosingSquare,
+        checkForWin
     }
 }
 
-const game = gameFlow('Jack', 'Bill')
+const displayController = (() => {
+    const square = document.querySelectorAll('.square')
+    const message = document.querySelector('.message')
+    const display = document.querySelector('.display')
+    const modal = document.querySelector('.modal')
 
-// const displayController = (() => {
-//     const square = Array.from(document.querySelectorAll('.square'))
+    const renderBoard = () => {
+        for(let i = 0; i < 9; i++) {
+            square[i].textContent = Gameboard.getValue(i)
+        }
+    }
     
-//     const choosingSquare = square.forEach(square => {
-//         square.addEventListener('click', (e) => {
-//             let index = e.target.dataset.index
-//             // getValue(index)
-//             // setValue(index, getMarker())
-//             console.log(index)
-//             square.textContent = index
-//         })
-//     })
-//     return {
-//         choosingSquare,
-//     }
+    const whosTurn = (player) => {
+        message.textContent = `It's ${player.getName()}'s turn`
+    }
 
-// })()
+    const displayWinner = (player) => {
+        display.textContent = `${player.getName()} has won!`
+    }
 
+    const displayDraw = () => {
+        display.textContent = `It's a draw!`
+    }
 
+    const openModal = () => {
+        modal.style.opacity = '1'
+        modal.style.visibility = 'visible'
+    }
 
+    const closeModal = () => {
+        modal.style.opacity = '0'
+        modal.style.visibility = 'hidden'
+    }
 
+    return {
+        renderBoard,
+        whosTurn,
+        displayWinner,
+        displayDraw,
+        openModal,
+        closeModal
+    }
+})()
 
-
-
+gameFlow('Player 1', 'Player 2')
